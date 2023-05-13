08debf59a09e7e14afc795f5b17ee2ae9cb79be1
Augusto Barbosa <augustobarbosa1988@gmail.com>
2023-05-13 20:04:51 -0300

    feat: middleware de erros para a aplicação

--- back-end/src/app.ts
+++ back-end/src/app.ts
@@ -1,13 +1,23 @@
-import express from 'express';
+import express, { NextFunction, Request, Response  } from 'express';
 import * as routers from './routes';
+import 'express-async-errors';
+// import { ZodError } from 'zod';
+import cors from 'cors';
 
 const app = express();
 
 app.use(express.json());
 
+app.use(cors())
+
 app.get('/health', (_req, res) => res.status(200).send('OK'));
 
 app.use('/products', routers.productRouter);
 
+// middleware de erros
+app.use((err: unknown, _: Request, res: Response, __: NextFunction) => {    
+    console.error(err);
+    return res.status(500).json({ message: 'Erro interno' });
+});
 
 export default app;
