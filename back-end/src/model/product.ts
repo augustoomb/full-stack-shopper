66e75678a50e389b5540aeeac1e0e01542dc5bbe
Augusto Barbosa <augustobarbosa1988@gmail.com>
2023-05-13 14:24:08 -0300

    feat: criando a model product

---
+++ back-end/src/model/product.ts
@@ -0,0 +1,20 @@
+import { RowDataPacket } from 'mysql2';
+import conn from '../database/connection';
+import Product from '../interfaces/product';
+import { SimpleModel } from './model';
+
+const DATABASE = 'shopper_db';
+
+export default class ProductModel implements SimpleModel<Product> {
+    constructor(private tableName: string = 'products', private connection = conn){}
+
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars, no-unused-vars
+    async list(): Promise<Partial<Product>[]> {
+        const result = await this.connection.execute(
+            `SELECT * FROM ${DATABASE}.${this.tableName}`
+        )
+
+        const [products] = result as RowDataPacket[];
+        return products as Product[];
+    }
+}
