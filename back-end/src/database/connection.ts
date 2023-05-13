28e093f1726c8e15724d343f4eb170c3cfddcc3a
Augusto Barbosa <augustobarbosa1988@gmail.com>
2023-05-13 14:20:21 -0300

    feat: configurando banco de dados

---
+++ back-end/src/database/connection.ts
@@ -0,0 +1,22 @@
+import dotenv from 'dotenv';
+import mysql from 'mysql2/promise';
+import { executeQueries, readQueries } from './queryUtils';
+dotenv.config();
+
+const conn = mysql.createPool({
+    host: process.env.MYSQLHOST,
+    user: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    port: Number(process.env.MYSQLPORT || 3306),
+})
+
+// SE ESTIVER EM AMBIENTE DE DESENVOLVIMENTO, VOU 'DROPAR' O BANCO E EXECUTAR MINHAS QUERIES DE TESTE
+if (['dev', 'development'].includes(process.env.NODE_ENV || 'development')) {
+    const dropQuery = readQueries('dropDatabase.sql');
+
+    executeQueries(conn, dropQuery).then(() => 
+        executeQueries(conn)
+    );
+}
+
+export default conn;
