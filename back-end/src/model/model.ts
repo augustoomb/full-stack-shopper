296a9d3ec9f4b7750472a7adcabfa0f56458f20a
Augusto Barbosa <augustobarbosa1988@gmail.com>
2023-05-13 14:21:48 -0300

    feat: criando interface para os models do projeto

---
+++ back-end/src/model/model.ts
@@ -0,0 +1,6 @@
+export interface SimpleModel<T> {
+    // create(obj: T): Promise<void>;
+    // create(obj: T): Promise<Partial<T>>;
+    // list(userId: number): Promise<Partial<T>[]>;
+    list(): Promise<Partial<T>[]>;
+}
