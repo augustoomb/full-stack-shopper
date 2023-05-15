24856a69b3e06437f34a190896f91ec000426bc4
Augusto Barbosa <augustobarbosa1988@gmail.com>
2023-05-15 13:05:26 -0300

    fix: editando interfaces para que os tipos suportem um array com mensagens de erro

--- back-end/src/interfaces/pack.ts
+++ back-end/src/interfaces/pack.ts
@@ -1,20 +1,7 @@
-import { z } from 'zod';
-
-const Pack = z.object({
-    id: z.number({
-        'invalid_type_error': '"id" deve ser um número',
-    }),
-    packId: z.number({
-        'invalid_type_error': '"packId" deve ser um número',
-    }),
-    productId: z.number({
-        'invalid_type_error': '"productId" deve ser um número',
-    }),
-    qty: z.number({
-        'invalid_type_error': '"qty" deve ser um número',
-    }),
-})
-
-type Pack = z.infer<typeof Pack>
-
-export default Pack;
+export default interface Pack {
+    id?: number;
+    packId?: number;
+    productId?: number;
+    qty?: number;
+    // messageErrors?: [];
+}
