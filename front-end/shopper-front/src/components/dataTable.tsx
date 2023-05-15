5630286910ee0ec28e112a0dd5ef9e75d3dd803c
Augusto Barbosa <augustobarbosa1988@gmail.com>
2023-05-15 15:23:32 -0300

    feat: montando dados dos produtos para serem exibidos na tela

--- front-end/shopper-front/src/components/dataTable.tsx
+++ front-end/shopper-front/src/components/dataTable.tsx
@@ -1,11 +1,32 @@
 interface dataTableProps {
-  
+  dataAPIProducts: any
 }
 
 export default function DataTable(props: dataTableProps) {
+
     return (
       <div>
-        Data Table
+        {
+          !props.dataAPIProducts ? (
+            <p>Sem dados</p>
+          ) : (
+            props.dataAPIProducts.map((item: any, index: number) => (
+              <div key={ index }>
+                <div>
+                  <p>Código: { item.data['product_code'] }</p>
+                  <p>Nome: { item.data['name'] }</p>
+                  <p>Preço Atual: { item.data['sales_price'] }</p>
+                  <p>Novo Preço: { item.data['new_price'] }</p>
+                </div>
+                {/* <div>
+                  <p>{ item.data['product_code'] }</p>
+                  <p>{ item.data['product_code'] }</p>
+                  <p>{ item.data['product_code'] }</p>
+                </div> */}
+              </div>
+            ))
+          )
+        }
       </div>
     )
 }
