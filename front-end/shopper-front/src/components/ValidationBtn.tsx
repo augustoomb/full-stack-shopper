060a72a2a1cd0fe449b0bf6fe98e3c399d3aac97
Augusto Barbosa <augustobarbosa1988@gmail.com>
2023-05-15 16:33:49 -0300

    feat: componente do botão que vai validar os dados do arquivo csv.

---
+++ front-end/shopper-front/src/components/ValidationBtn.tsx
@@ -0,0 +1,28 @@
+import { validateProduct } from '../services/products';
+
+interface ValidationBtnProps {
+  dataCsvFile: (any)[]
+  setDataAPIProducts: any
+}
+
+export default function ValidationBtn(props: ValidationBtnProps) {
+
+    async function validateData() {
+      const stringfyObjCsv = JSON.stringify(props.dataCsvFile);
+      props.setDataAPIProducts(await validateProduct(stringfyObjCsv));
+    }
+
+    return (
+      <button
+        disabled={ props.dataCsvFile.length === 0 }
+        onClick={ validateData }
+      >
+          <h2>
+            Validar <span>-&gt;</span>
+          </h2>
+          <p>
+            Valide os dados do seu arquivo antes de salvar os dados no banco.
+          </p>
+      </button>  
+    )
+}
