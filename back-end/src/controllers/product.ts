a2560f66ad279663d37e4aa5a9e65ae49d857db0
Augusto Barbosa <augustobarbosa1988@gmail.com>
2023-05-15 19:10:46 -0300

    feat: criando controller para update de product

--- back-end/src/controllers/product.ts
+++ back-end/src/controllers/product.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import { ProductService } from '../services/product';
+import { StatusCodes } from 'http-status-codes';
 
 export async function list(req: Request, res: Response) {
     const productService = new ProductService();
@@ -18,4 +19,21 @@ export async function validateProduct(req: Request, res: Response) {
     const arrCheckedProducts = await productService.validate(arrProducts)
 
     return res.json(arrCheckedProducts)
+}
+
+export async function update(req: Request, res: Response) {
+    const data = req.body;
+
+    const arrProducts = JSON.parse(data.product);
+
+    const productService = new ProductService();
+
+    // await productService.update(arrProducts[0].code, arrProducts[0].price)
+
+    Promise.all(arrProducts.map(async (product: any) => {
+        // const completeProduct = await this.productExists(product)
+        await productService.update(product.code, product.price)
+    }))
+
+    res.status(StatusCodes.OK).send(true);
 }
