28f0b3a5b0e9bfdd3174f8c3474c9d74505bc0d9
Augusto Barbosa <augustobarbosa1988@gmail.com>
2023-05-15 13:08:06 -0300

    feat: criando a rota de validação de product

--- back-end/src/routes/product.ts
+++ back-end/src/routes/product.ts
@@ -4,6 +4,6 @@ import { list, validateProduct } from '../controllers/product';
 const productRouter = Router();
 
 productRouter.get('/', list)
-productRouter.get('/validate', validateProduct)
+productRouter.post('/validate', validateProduct)
 
 export default productRouter;
