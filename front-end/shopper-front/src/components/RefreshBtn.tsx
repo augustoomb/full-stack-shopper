5e559004db45e2ded92bb34e150142a4b922231e
Augusto Barbosa <augustobarbosa1988@gmail.com>
2023-05-15 17:42:28 -0300

    feat: botão de atualizar

--- front-end/shopper-front/src/components/RefreshBtn.tsx
+++ front-end/shopper-front/src/components/RefreshBtn.tsx
@@ -1,16 +1,44 @@
-export default function RefreshBtn() {
+interface RefreshBtnProps {
+  dataAPIProducts: any
+}
+
+export default function RefreshBtn(props: RefreshBtnProps) {
+    const checkContentErrors = () => {
+      return props.dataAPIProducts.some((item:any) => item.contentErrors.length > 0);
+    }
+    
+    const checkRulesErrors = () => {
+      return props.dataAPIProducts.some((item:any) => item.rulesErrors.length > 0);
+    }
+
+    const checkTypeErrors = () => {
+      return props.dataAPIProducts.some((item:any) => item.typeErrors.length > 0);
+    }
+
+    const checkErrors = () => {
+      if(!props.dataAPIProducts) {
+        return true
+      }
+      
+      if(
+        checkContentErrors() ||
+        checkRulesErrors()  ||
+        checkTypeErrors()
+      ) {
+        return true
+      }
+    }
+
     return (
-      <a
-      href=""
-      target="_blank"
-      rel="noopener noreferrer"
-    >
-      <h2>
-        Atualizar <span>-&gt;</span>
-      </h2>
-      <p>
-        Atualize os dados no banco
-      </p>
-    </a>  
+      <button
+          disabled={ checkErrors() }
+        >
+        <h2>
+          Atualizar <span>-&gt;</span>
+        </h2>
+        <p>
+          Atualize os dados no banco
+        </p>
+      </button>  
     )
 }
